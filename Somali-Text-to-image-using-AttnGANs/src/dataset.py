d8523d2eefc8df501880048c98fa69bf8c68dbab
Fahmaan114 <118554257+Fahmaan114@users.noreply.github.com>
2023-08-06 17:50:41 +0300

    dataset.py

--- Somali-Text-to-image-using-AttnGANs/src/dataset.py
+++ Somali-Text-to-image-using-AttnGANs/src/dataset.py
@@ -131,12 +131,12 @@ class TextDataset(data.Dataset):
 
     def load_bbox(self):
         data_dir = self.data_dir
-        bbox_path = os.path.join(data_dir, "CUB_200_2011/bounding_boxes.txt")
+        bbox_path = os.path.join(data_dir, "birds/CUB_200_2011/bounding_boxes.txt")
         df_bounding_boxes = pd.read_csv(
             bbox_path, delim_whitespace=True, header=None
         ).astype(int)
         #
-        filepath = os.path.join(data_dir, "CUB_200_2011/images.txt")
+        filepath = os.path.join(data_dir, "birds/CUB_200_2011/images.txt")
         df_filenames = pd.read_csv(filepath, delim_whitespace=True, header=None)
         filenames = df_filenames[1].tolist()
         print("Total filenames: ", len(filenames), filenames[0])
